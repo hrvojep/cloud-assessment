305c20dceea9db7e49b42e5b78db594f24e218c9
Eloise Aitken <eloise_aitken@hotmail.com>
2018-10-04 11:09:42 +1000

    inital e2e tests for button panel

--- assess/e2e/app.e2e-spec.ts
+++ assess/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
 describe('cloud-assessment App', () => {
   let page: AppPage;
@@ -7,8 +8,46 @@ describe('cloud-assessment App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  //Header
+  it('should display the app title in the header', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getAppTitle()).toEqual('Cloud Assessment Tool beta');
   });
+
+  //Home Page
+  it('should display a home button', () => {
+    page.navigateTo();
+    expect(page.getHomeButton().getText()).toEqual('Home');
+  });
+
+  it('should route to the home page when the home button is clicked', () => {
+    page.navigateTo();
+    page.getHomeButton().click();
+    expect(page.getHomeTitleText()).toEqual('Start an assessment');
+  });
+
+  //Prepare Page
+  it('should display a prepare button', () => {
+    page.navigateTo();
+    expect(page.getPrepareButton().getText()).toEqual('Prepare');
+  });
+
+  it('should route to the prepare page when the prepare button is clicked', () => {
+    page.navigateTo();
+    page.getPrepareButton().click();
+    expect(page.getPrepareTitleText()).toEqual('Preparing for an assessment');
+  });
+
+  //About Page
+  it('should display an about button', () => {
+    page.navigateTo();
+    expect(page.getAboutButton().getText()).toEqual('About');
+  });
+
+  it('should route to the about page when the about button is clicked', () => {
+    page.navigateTo();
+    page.getAboutButton().click();
+    expect(page.getAboutTitleText()).toEqual('About');
+  });
+
 });
